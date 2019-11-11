c46325a97406a6bd11350fcd68248484ec0786bc
Mario Bonilla <mario.bonilla@reale.cl>
2019-11-11 14:45:06 -0300

    Clase 54 - Heroes diseño con ngFor

--- angular-udemy/fernando_herrera/02-spa/src/app/components/buscador/buscador.component.ts
+++ angular-udemy/fernando_herrera/02-spa/src/app/components/buscador/buscador.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-buscador',
-  templateUrl: './buscador.component.html',
-  styleUrls: ['./buscador.component.css']
+  templateUrl: './buscador.component.html'
 })
 export class BuscadorComponent implements OnInit {
 
