8684ced81e0d1831dc537df3a201a7703fc90887
Mario Bonilla <mariobonillam@gmail.com>
2019-11-18 23:37:17 -0300

    Clase 64 - @Input

--- angular-udemy/fernando_herrera/02-spa/src/app/app.module.ts
+++ angular-udemy/fernando_herrera/02-spa/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { AppRoutingModule } from './app-routing.module';
     AboutComponent,
     HeroesComponent,
     HeroeComponent,
-    BuscadorComponent
+    BuscadorComponent,
+    HeroeTarjetaComponent
   ],
   imports: [
     BrowserModule,
