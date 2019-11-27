57bb9fd54a39fda85e1e3e815e88624e59ed97f8
mario-coder <mariobonillam@gmail.com>
2019-11-26 23:05:53 -0300

    Clase 186 - Obteniendo info del formulario

--- angular-udemy/fernando_herrera/08-formularios/src/app/components/template/template.component.ts
+++ angular-udemy/fernando_herrera/08-formularios/src/app/components/template/template.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styles: []
 })
-export class TemplateComponent implements OnInit {
+export class TemplateComponent {
+
+  usuario = {
+    nombre: 'Mario',
+    apellido: 'Bonilla',
+    correo: 'm@mail.cl'
+  };
 
   constructor() { }
 
-  ngOnInit() {
+  guardar(forma: NgForm){
+    console.log('Formulario posteado');
+    console.log('NgForm: ', forma);
+    console.log('Valor', forma.value);
   }
 
 }
