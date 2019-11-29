b753dea3fcc45dd5b3ad8640b9b9ddc1a08c98a9
mario-coder <mariobonillam@gmail.com>
2019-11-29 00:09:57 -0300

    Clase 195 - Data: Validaciones y html

--- angular-udemy/fernando_herrera/08-formularios/src/app/components/data/data.component.ts
+++ angular-udemy/fernando_herrera/08-formularios/src/app/components/data/data.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-data',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  forma: FormGroup;
+  emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$";
+
+  constructor() {
+    this.forma = new FormGroup({
+      'nombre' : new FormControl('', Validators.required),
+      'apellido' : new FormControl('', [Validators.required, Validators.minLength(3)]),
+      'correo' : new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)])
+    });
+   }
+
+   guardarCambios(){
+    console.log(this.forma.value);
+   }
 
   ngOnInit() {
   }
