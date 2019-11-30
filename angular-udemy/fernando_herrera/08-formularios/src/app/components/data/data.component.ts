b7a6d1340d4b3872618a8d1311839620ba36b613
Mario Bonilla <mariobonillam@gmail.com>
2019-11-30 15:45:47 -0300

    Clase 196 - Agrupaciones de objetos en formulario

--- angular-udemy/fernando_herrera/08-formularios/src/app/components/data/data.component.ts
+++ angular-udemy/fernando_herrera/08-formularios/src/app/components/data/data.component.ts
@@ -1,30 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-data',
-  templateUrl: './data.component.html',
+  selector: "app-data",
+  templateUrl: "./data.component.html",
   styles: []
 })
 export class DataComponent implements OnInit {
-
   forma: FormGroup;
-  emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$";
+  usuario: Object = {
+    nombreCompleto: {
+      nombre: "mario",
+      apellido: "bonilla"
+    },
+    correo: "mario@mario.cl"
+  };
+  emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$";
 
   constructor() {
     this.forma = new FormGroup({
-      'nombre' : new FormControl('', Validators.required),
-      'apellido' : new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'correo' : new FormControl('', [Validators.required, Validators.pattern(this.emailPattern)])
+      nombreCompleto: new FormGroup({
+        nombre: new FormControl("", [
+          Validators.required,
+          Validators.minLength(3)
+        ]),
+        apellido: new FormControl("", [
+          Validators.required,
+          Validators.minLength(3)
+        ])
+      }),
+      correo: new FormControl("", [
+        Validators.required,
+        Validators.pattern(this.emailPattern)
+      ])
     });
-   }
-
-   guardarCambios(){
-    console.log(this.forma.value);
-   }
-
-  ngOnInit() {
   }
 
+  guardarCambios() {
+    console.log(this.forma.value);
+  }
+
+  ngOnInit() {}
 }
