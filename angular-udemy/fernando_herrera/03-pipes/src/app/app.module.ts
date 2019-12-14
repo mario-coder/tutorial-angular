d67928b76193e1acdda39b2ea6e43d42fe96cd13
Mario Bonilla <mariobonillam@gmail.com>
2019-12-14 17:14:55 -0300

    Clase 85 - Seccion pipes terminada

--- angular-udemy/fernando_herrera/03-pipes/src/app/app.module.ts
+++ angular-udemy/fernando_herrera/03-pipes/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { LOCALE_ID, NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+
+import localeEs from "@angular/common/locales/es";
+import { registerLocaleData } from "@angular/common";
+import { CapitalizadoPipe } from "./pipes/capitalizado.pipe";
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+
+registerLocaleData(localeEs);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  imports: [BrowserModule],
+  declarations: [AppComponent, CapitalizadoPipe, DomseguroPipe, ContrasenaPipe],
+  providers: [{ provide: LOCALE_ID, useValue: "es" }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular-udemy/fernando_herrera/03-pipes/src/app/pipes/contrasena.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "contrasena"
+})
+export class ContrasenaPipe implements PipeTransform {
+  transform(value: any, activar: boolean = true): any {
+    if (activar) {
+      let salida: string = "";
+      for (let i = 0; i < value.length; i++) {
+        salida += "*";
+      }
+
+      return salida;
+    } else {
+      return value;
+    }
+  }
+}
