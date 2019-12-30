92ed1b3e68ea9b96b2ea94f9f2eb5e55e52a682b
Mario Bonilla <mario.bonilla@reale.cl>
2019-12-30 16:20:13 -0300

    Agrega export al datatable

--- angular-udemy/fernando_herrera/AngularDemo/src/app/components/datatable/datatable.component.ts
+++ angular-udemy/fernando_herrera/AngularDemo/src/app/components/datatable/datatable.component.ts
@@ -17,10 +17,46 @@ export class DatatableComponent implements OnInit {
     { field: 'color', header: 'Color' }
 ];
 
+  exportColumns: any[];
+
   constructor(private carService: CarService) { }
 
   ngOnInit() {
     this.cars = this.carService.getCarsLarge()//.then(cars => this.cars = cars);
+
+    this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
   }
+
+  exportPdf() {
+    import("jspdf").then(jsPDF => {
+        import("jspdf-autotable").then(x => {
+            const doc = new jsPDF.default(0,0);
+            //console.log('cars pdf '+ this);
+            doc.autoTable(this.exportColumns, this.cars);
+            doc.save('primengTable.pdf');
+        })
+    })
+  }
+
+  exportExcel() {
+      import("xlsx").then(xlsx => {
+          const worksheet = xlsx.utils.json_to_sheet(this.cars);
+          const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+          const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+          this.saveAsExcelFile(excelBuffer, "primengTable");
+      });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+      import("file-saver").then(FileSaver => {
+          let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+          let EXCEL_EXTENSION = '.xlsx';
+          const data: Blob = new Blob([buffer], {
+              type: EXCEL_TYPE
+          });
+          FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+      });
+  }
+
 
 }
