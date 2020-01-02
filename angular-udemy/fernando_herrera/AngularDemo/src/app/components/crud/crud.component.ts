99b924ead7c5beec9109e25a466573062e1325ee
Mario Bonilla <mario.bonilla@reale.cl>
2020-01-02 14:39:15 -0300

    Mejora tabla CRUD con export de data

--- angular-udemy/fernando_herrera/AngularDemo/src/app/components/crud/crud.component.ts
+++ angular-udemy/fernando_herrera/AngularDemo/src/app/components/crud/crud.component.ts
@@ -21,6 +21,8 @@ export class CrudComponent implements OnInit {
 
   cols: any[];
 
+  exportColumns: any[];
+
   constructor(private carService: CarService) { }
 
   ngOnInit() {
@@ -32,7 +34,10 @@ export class CrudComponent implements OnInit {
           { field: 'brand', header: 'Brand' },
           { field: 'color', header: 'Color' }
       ];
-  }
+  
+  
+      this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
+    }
 
   showDialogToAdd() {
       this.newCar = true;
@@ -71,5 +76,36 @@ export class CrudComponent implements OnInit {
           car[prop] = c[prop];
       }
       return car;
+  }
+
+  exportPdf() {
+    import("jspdf").then(jsPDF => {
+        import("jspdf-autotable").then(x => {
+            const doc = new jsPDF.default(0,0);
+            //console.log('cars pdf '+ this);
+            doc.autoTable(this.exportColumns, this.cars);
+            doc.save('primengTable.pdf');
+        })
+    })
+  }
+
+  exportExcel() {
+      import("xlsx").then(xlsx => {
+          const worksheet = xlsx.utils.json_to_sheet(this.cars);
+          const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+          const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+          this.saveAsExcelFile(excelBuffer, "primengTable");
+      });
+  }
+
+  saveAsExcelFile(buffer: any, fileName: string): void {
+      import("file-saver").then(FileSaver => {
+          let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+          let EXCEL_EXTENSION = '.xlsx';
+          const data: Blob = new Blob([buffer], {
+              type: EXCEL_TYPE
+          });
+          FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+      });
   }
 }
