c6781772642d69a1a1d39a1f356f4634109ed147
mario-coder <mariobonillam@gmail.com>
2020-01-07 00:01:53 -0300

    Avances formulario forma de pago

--- angular-udemy/fernando_herrera/AngularDemo/src/app/components/propuesta/propuesta.component.ts
+++ angular-udemy/fernando_herrera/AngularDemo/src/app/components/propuesta/propuesta.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./propuesta.component.css']
 })
 export class PropuestaComponent implements OnInit {
+  FORMA_PAGO_PAC:number = 1;
+  FORMA_PAGO_PAT:number = 2;
+  FORMA_PAGO_DIRECTO:number = 3;
 
-  constructor() { }
+  formaPagoSelected : number;
+  
+  FORMAS_PAGO:any[] = [
+    {label: "PAC", code: this.FORMA_PAGO_PAC}, 
+    {label: "PAT", code: this.FORMA_PAGO_PAT}, 
+    {label: "PAGO DIRECTO", code: this.FORMA_PAGO_DIRECTO}]
+    
+    constructor() { }
+    
+    ngOnInit() {
+      this.formaPagoSelected = this.FORMA_PAGO_PAC;
+    }
 
-  ngOnInit() {
+  cambioFormaPago() {
+    
   }
-
 }
