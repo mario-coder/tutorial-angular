7ec4a2461eade325f2e8629439cadab299932c79
mario-coder <mariobonillam@gmail.com>
2020-01-07 22:46:59 -0300

    Mejora formulario reactivo

--- angular-udemy/fernando_herrera/08-formularios/src/app/components/data/data.component.ts
+++ angular-udemy/fernando_herrera/08-formularios/src/app/components/data/data.component.ts
@@ -1,63 +1,30 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
-import { Observable } from "rxjs/Rx";
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: "app-data",
-  templateUrl: "./data.component.html",
-  styles: []
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.css']
 })
 export class DataComponent implements OnInit {
   forma: FormGroup;
-  usuario: any = {
-    nombreCompleto: {
-      nombre: "mario",
-      apellido: "bonilla"
-    },
-    correo: "mario@mario.cl",
-    pasatiempos: ["Comer", "Dormir", "Correr"]
-  };
-  emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$";
-
-  constructor() {
+  
+  ngOnInit() {
     this.forma = new FormGroup({
       nombreCompleto: new FormGroup({
-        nombre: new FormControl("", [
-          Validators.required,
-          Validators.minLength(3)
-        ]),
-        apellido: new FormControl("", [
-          Validators.required,
-          Validators.minLength(3),
-          this.noHerrera
-        ])
+        nombre: new FormControl("", [Validators.required, Validators.minLength(3)]),
+        apellido: new FormControl("", [Validators.required, Validators.minLength(3), this.noHerrera])
       }),
-      correo: new FormControl("", [
-        Validators.required,
-        Validators.pattern(this.emailPattern)
-      ]),
+      correo: new FormControl("", [Validators.required, Validators.email]),
       pasatiempos: new FormArray([new FormControl("", [Validators.required])]),
       username: new FormControl("", [Validators.required], this.existeUsuario),
-      password1: new FormControl("", [Validators.required]),
-      password2: new FormControl()
+      password1: new FormControl("", [Validators.required])
     });
+    this.forma.addControl('password2', new FormControl("", [Validators.required, this.noIgual.bind(this)]))
+  }
 
-    this.forma.controls["password2"].setValidators([
-      Validators.required,
-      this.noIgual.bind(this.forma)
-    ]);
-
-    //Validacion cambio de valor de un campo
-    //this.forma.valueChanges.subscribe(data => {
-    this.forma.get("nombreCompleto.nombre").valueChanges.subscribe(data => {
-      console.log(data);
-    });
-
-    //Validacion cambio de estado de un campo
-    this.forma.get("nombreCompleto.nombre").statusChanges.subscribe(data => {
-      console.log(data);
-    });
-    //this.forma.setValue(this.usuario);
+  constructor() {
   }
 
   agregarPasatiempo() {
@@ -75,17 +42,12 @@ export class DataComponent implements OnInit {
     return control.value === "herrera" ? { noherrera: false } : null;
   }
 
-  noIgual(control: FormControl) {
-    let forma = this;
-    return control.value !== forma.controls["password1"].value
-      ? { noiguales: true }
-      : null;
-  }
-
+  noIgual = (control: FormControl) => control.value !== this.forma.controls["password1"].value? { noiguales: true } : null;
+  
   existeUsuario(control: FormControl): Promise<any> | Observable<any> {
     let promesa = new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (control.value === "strider") {
+        if (control.value === "mario") {
           resolve({ existe: true });
         } else {
           resolve(null);
@@ -94,6 +56,4 @@ export class DataComponent implements OnInit {
     });
     return promesa;
   }
-
-  ngOnInit() {}
 }
--- angular-udemy/fernando_herrera/AngularDemo/src/app/components/formularios/formularios.component.ts
+++ angular-udemy/fernando_herrera/AngularDemo/src/app/components/formularios/formularios.component.ts
@@ -9,42 +9,22 @@ import { Observable } from 'rxjs';
 })
 export class FormulariosComponent implements OnInit {
   forma: FormGroup;
-  usuario: any = {
-    nombreCompleto: {
-      nombre: "mario",
-      apellido: "bonilla"
-    },
-    correo: "mario@mario.cl",
-    pasatiempos: ["Comer", "Dormir", "Correr"]
-  };
-  emailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$";
-
-  constructor() {
+  
+  ngOnInit() {
     this.forma = new FormGroup({
       nombreCompleto: new FormGroup({
         nombre: new FormControl("", [Validators.required, Validators.minLength(3)]),
         apellido: new FormControl("", [Validators.required, Validators.minLength(3), this.noHerrera])
       }),
-      correo: new FormControl("", [Validators.required, Validators.pattern(this.emailPattern)]),
+      correo: new FormControl("", [Validators.required, Validators.email]),
       pasatiempos: new FormArray([new FormControl("", [Validators.required])]),
       username: new FormControl("", [Validators.required], this.existeUsuario),
-      password1: new FormControl("", [Validators.required]),
-      password2: new FormControl()
+      password1: new FormControl("", [Validators.required])
     });
+    this.forma.addControl('password2', new FormControl("", [Validators.required, this.noIgual.bind(this)]))
+  }
 
-    this.forma.controls["password2"].setValidators([Validators.required, this.noIgual.bind(this.forma)]);
-
-    //Validacion cambio de valor de un campo
-    //this.forma.valueChanges.subscribe(data => {
-    this.forma.get("nombreCompleto.nombre").valueChanges.subscribe(data => {
-      console.log(data);
-    });
-
-    //Validacion cambio de estado de un campo
-    this.forma.get("nombreCompleto.nombre").statusChanges.subscribe(data => {
-      console.log(data);
-    });
-    //this.forma.setValue(this.usuario);
+  constructor() {
   }
 
   agregarPasatiempo() {
@@ -62,18 +42,12 @@ export class FormulariosComponent implements OnInit {
     return control.value === "herrera" ? { noherrera: false } : null;
   }
 
-  noIgual(control: FormControl) {
-    console.table(this)
-    let forma = this;
-    return control.value !== forma.controls["password1"].value
-      ? { noiguales: true }
-      : null;
-  }
-
+  noIgual = (control: FormControl) => control.value !== this.forma.controls["password1"].value? { noiguales: true } : null;
+  
   existeUsuario(control: FormControl): Promise<any> | Observable<any> {
     let promesa = new Promise((resolve, reject) => {
       setTimeout(() => {
-        if (control.value === "strider") {
+        if (control.value === "mario") {
           resolve({ existe: true });
         } else {
           resolve(null);
@@ -82,6 +56,4 @@ export class FormulariosComponent implements OnInit {
     });
     return promesa;
   }
-
-  ngOnInit() {}
 }
