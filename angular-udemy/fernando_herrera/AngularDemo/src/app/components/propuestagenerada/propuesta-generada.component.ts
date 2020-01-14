0d93b72bd68e3fc8ebbafdda9462f650055e4ed3
Mario Bonilla <mario.bonilla@reale.cl>
2020-01-14 09:53:27 -0300

    reposicion componentes

---
+++ angular-udemy/fernando_herrera/AngularDemo/src/app/components/propuestagenerada/propuesta-generada.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-propuesta-generada',
+  templateUrl: './propuesta-generada.component.html',
+  styleUrls: ['./propuesta-generada.component.css']
+})
+export class PropuestaGeneradaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
