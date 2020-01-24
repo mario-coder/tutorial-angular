ec72f1b3a86e3f0890b20a44b653b34e5f666de1
mario-coder <mariobonillam@gmail.com>
2020-01-23 23:51:29 -0300

    Continuacion cambio de elementos por componentes PrimeNG
    Mejoras estilos

--- angular-udemy/fernando_herrera/cotizadorAngular/src/app/modulos/livianos/componentes/propuesta-livianos/propuesta-livianos.component.ts
+++ angular-udemy/fernando_herrera/cotizadorAngular/src/app/modulos/livianos/componentes/propuesta-livianos/propuesta-livianos.component.ts
@@ -10,7 +10,10 @@ import { DatePickerConfigService } from 'src/app/shared/config/date-picker-confi
   styleUrls: ['./propuesta-livianos.component.css']
 })
 export class PropuestaLivianosComponent implements OnInit {
-  tiposDocumento:any[];
+  TIPOS_DOCUMENTO:any[] = [
+    { label: "PERSONA NATURAL", code: "1" },
+    { label: "PERSONA JURIDICA", code: "2" }
+  ];
 
   FORMA_PAGO_PAC:string = "1";
   FORMA_PAGO_PAT:string = "2";
@@ -22,9 +25,16 @@ export class PropuestaLivianosComponent implements OnInit {
   bsConfig: Partial<BsDatepickerConfig>;
   dateCustomClasses: DatepickerDateCustomClasses[];
 
-  respuestaPreexistenciasSelected : number;
-  respuestaAccesoriosSelected : number;
+  preexistenciasSelected : any;
+  accesoriosSelected : any;
+  modalidadIngresoSelected : any;
+  respuestaMediosNotificacionSelected : any;
   formaPagoSelected : any;
+  tipoCuentaSelected : any;
+  bancoSelected : any;
+  numeroCuotasSelected : any;
+  tipoTarjetaSelected : any;
+  diaPagoSelected : any;
   obtenerTotalesSelected:boolean = false;
   propuestaGeneradaSelected:boolean = false;
 
@@ -32,8 +42,57 @@ export class PropuestaLivianosComponent implements OnInit {
     {label: "PAC", code: this.FORMA_PAGO_PAC}, 
     {label: "PAT", code: this.FORMA_PAGO_PAT}, 
     {label: "PAGO DIRECTO", code: this.FORMA_PAGO_DIRECTO}]
-  comunas: any[];
+
+  RESPUESTAS_SI_NO:any[] = [
+    {label: "NO", code: this.RESPUESTA_NO},
+    {label: "SI", code: this.RESPUESTA_SI}]
+  
+  COMUNAS: any[] = [
+    { label: "LAS CONDES", code: "1" },
+    { label: "PROVIDENCIA", code: "2" }
+  ];
     
+  MEDIOS_NOTIFICACION:any[] = [
+    {label: "EMAIL", code: "1"},
+    {label: "TELEFONO", code: "2"}
+  ]
+
+  NUMERO_CUOTAS:any[] = [
+    {label: "1", code: "1"},
+    {label: "2", code: "2"},
+    {label: "3", code: "3"},
+    {label: "4", code: "4"},
+    {label: "5", code: "5"}
+  ]
+
+  DIAS_PAGO:any[] = [
+    {label: "5", code: "5"},
+    {label: "20", code: "20"}
+  ]
+
+  MODALIDADES_INGRESO:any[] = [
+    {label: "FACTURA", code: "1"},
+    {label: "INSPECCION", code: "2"}
+  ];
+
+  TIPOS_CUENTA:any[] = [
+    {label: "CORRIENTE", code: "1"},
+    {label: "VISTA", code: "2"}
+  ]
+
+  TIPOS_TARJETA:any[] = [
+    {label: "VISA", code: "1"},
+    {label: "MASTERCARD", code: "2"}
+  ]
+
+  BANCOS: any[] = [
+    {label: "BCI", code: "1" },
+    {label: "BICE", code: "2" },
+    {label: "CHILE", code: "3" },
+    {label: "Santander", code: "4" },
+    {label: "Scotiabank", code: "5" }
+  ];
+
     constructor(private router: Router,
       private datePickerConfig: DatePickerConfigService) { }
     
@@ -44,16 +103,12 @@ export class PropuestaLivianosComponent implements OnInit {
       this.dateCustomClasses = this.datePickerConfig.dateCustomClasses;
 
       this.formaPagoSelected = this.FORMAS_PAGO[0];
+      this.modalidadIngresoSelected = this.MODALIDADES_INGRESO[0];
+      //this.numeroCuotasSelected = this.NUMERO_CUOTAS[0];
+      this.preexistenciasSelected = this.RESPUESTAS_SI_NO[0];
+      this.accesoriosSelected = this.RESPUESTAS_SI_NO[0];
+      this.respuestaMediosNotificacionSelected = this.MEDIOS_NOTIFICACION[0];
 
-      this.tiposDocumento = [
-        { label: "PERSONA NATURAL", code: "1" },
-        { label: "PERSONA JURIDICA", code: "2" }
-      ];
-
-      this.comunas = [
-        { label: "LAS CONDES", code: "1" },
-        { label: "PROVIDENCIA", code: "2" }
-      ];
     }
 
   obtenerTotales() {
