1938fd748eb6543df235d7a67ed5e4517994ad15
Mario Bonilla <mario.bonilla@reale.cl>
2020-02-04 18:04:57 -0300

    Creacion componente grilla seleccion de cotizadaciones

--- angular-udemy/fernando_herrera/cotizadorAngular/src/app/modules/livianos/componentes/simulacion-livianos/grilla-tarifas-livianos/grilla-tarifas-livianos.component.ts
+++ angular-udemy/fernando_herrera/cotizadorAngular/src/app/modules/livianos/componentes/simulacion-livianos/grilla-tarifas-livianos/grilla-tarifas-livianos.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2, ChangeDetectorRef, Input } from '@angular/core';
 
 @Component({
   selector: 'app-grilla-tarifas-livianos',
   templateUrl: './grilla-tarifas-livianos.component.html',
-  styleUrls: ['./grilla-tarifas-livianos.component.css']
+  styleUrls: [
+'../../../../../shared/styles/simulacion/seleccion-tarifas/seleccion-tarifas.css']
 })
 export class GrillaTarifasLivianosComponent implements OnInit {
 
-  constructor() { }
+  headerColumnas: boolean[] = [];
+  headerFilas: boolean[] = [];
 
+  @Input() deducibleTarifaSeleccionada: number;
+  @Input() planTarifaSeleccionada: number;
+  @Input() tarifas: any[];
+  @Input() deducibles: any[];
+  @Input() productos: any[];
+
+  filaTarifaSeleccionadaOld: number;
+  columnaTarifaSeleccionadaOld: number;
+
+  filaTarifaSeleccionadaNew: number;
+  columnaTarifaSeleccionadaNew: number;
+
+  tarifasClasificadas : any[] = [];
+  _tarificado: boolean = false;
+
+  constructor(private renderer: Renderer2, private elem: ElementRef, private cd : ChangeDetectorRef){}
+  
   ngOnInit() {
+
+
+
+
+
+
+    this.cd.detectChanges();
   }
 
+  ordena(simA: { codigoDeducible: number; }, simB: { codigoDeducible: number; }){ return (simA.codigoDeducible - simB.codigoDeducible)}
+
+  mapeoTarifas(simulaciones: any) {
+    let tarifasClasificadas: any = {};
+    let tarifasXL: any[] = [];
+    let tarifasL: any[] = [];
+    let tarifasM: any[] = [];
+    let tarifasS: any[] = [];
+
+    let tarifaXL_Part = "1";
+    let tarifaL_Part = "2";
+    let tarifaM_Part = "3";
+    let tarifaS_Part = "4";
+
+    simulaciones.map(simulacion => {
+      switch(simulacion.codigoActividad){
+        case tarifaXL_Part:
+          tarifasXL.push(simulacion)
+        break;
+        case tarifaL_Part:
+          tarifasL.push(simulacion)
+        break;
+        case tarifaM_Part:
+          tarifasM.push(simulacion)
+        break;
+        case tarifaS_Part:
+          tarifasS.push(simulacion)
+        break;
+      }
+    });
+
+    tarifasXL.sort(this.ordena)
+    tarifasL.sort(this.ordena)
+    tarifasM.sort(this.ordena)
+    tarifasS.sort(this.ordena)
+    
+    tarifasClasificadas = {
+      tarifasXL,
+      tarifasL,
+      tarifasM,
+      tarifasS
+    }
+
+    this._tarificado = true;
+    return tarifasClasificadas
+  }
+
+  selecciona(fila, columna) {
+    console.log(fila, columna)
+  }
 }
