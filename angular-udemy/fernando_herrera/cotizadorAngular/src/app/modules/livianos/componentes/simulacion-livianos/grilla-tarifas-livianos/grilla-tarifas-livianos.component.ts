bae32710e008347e103be3018dfba3bec1b1106c
Mario Bonilla <mario.bonilla@reale.cl>
2020-02-05 10:48:06 -0300

    Grilla Tarifas Perfeccionada

--- angular-udemy/fernando_herrera/cotizadorAngular/src/app/modules/livianos/componentes/simulacion-livianos/grilla-tarifas-livianos/grilla-tarifas-livianos.component.ts
+++ angular-udemy/fernando_herrera/cotizadorAngular/src/app/modules/livianos/componentes/simulacion-livianos/grilla-tarifas-livianos/grilla-tarifas-livianos.component.ts
@@ -14,15 +14,15 @@ export class GrillaTarifasLivianosComponent implements OnInit {
   @Input() planTarifaSeleccionada: number;
   @Input() deducibleTarifaSeleccionada: number;
   @Input() simulaciones: any[];
+  deduciblesExistentes: any[] = [];
+  planesExistentes: any[] = [];
 
   _filaTarifaSeleccionada: number;
   _columnaTarifaSeleccionada: number;
 
-  tarifasClasificadas : any = {};
+  tarifasClasificadas : any[] = [];
   arrayTarifas : any[] = [];
   
-  deduciblesExistentes: any[] = [];
-  planesExistentes: any[] = [];
 
   constructor(private renderer: Renderer2, private elem: ElementRef, private cd : ChangeDetectorRef){}
   
@@ -38,7 +38,7 @@ export class GrillaTarifasLivianosComponent implements OnInit {
     this.cd.detectChanges();
   }
 
-  ordenaPorDeducible(simA: { codigoDeducible: number; }, simB: { codigoDeducible: number; }){ return (simA.codigoDeducible - simB.codigoDeducible)}
+  ordenaPorCodigoDeducible(simA: { codigoDeducible: number; }, simB: { codigoDeducible: number; }){ return (simA.codigoDeducible - simB.codigoDeducible)}
   ordenaPorCodigoActividad(groupSimsA: { codigoActividad: number; }, groupSimsB: { codigoActividad: number; }){ return (groupSimsA.codigoActividad - groupSimsB.codigoActividad)}
 
   mapeoTarifas(simulaciones: any) {
@@ -46,39 +46,65 @@ export class GrillaTarifasLivianosComponent implements OnInit {
     simulaciones.sort(this.ordenaPorCodigoActividad);
 
     simulaciones.map(simulacion => {
-      if(this.tarifasClasificadas[simulacion.codigoActividad] == undefined) {
+      if(this.tarifasClasificadas[simulacion.codigoActividad] === undefined) {
         this.tarifasClasificadas[simulacion.codigoActividad] = []
       }
       
+      this.guardarDeducibleExistente(simulacion)
+      this.guardarPlanExistente(simulacion)
+
       this.tarifasClasificadas[simulacion.codigoActividad].push(simulacion);
     });
 
-    let actividadActual = ""
-    simulaciones.map(simulacion => {
 
-      if(!this.planesExistentes.includes(simulacion.codigoActividad)){
-        this.tarifasClasificadas[simulacion.codigoActividad].sort(this.ordenaPorDeducible);
-        
-        this.planesExistentes.push(simulacion.codigoActividad);
-  
+    this.tarifasClasificadas.map(listaTarifas => {
+
+      let simulacionExt = (listaTarifas && listaTarifas.length > 0)? listaTarifas[0] : undefined;
+
+      if(simulacionExt) {
+        this.tarifasClasificadas[simulacionExt.codigoActividad].sort(this.ordenaPorCodigoDeducible);
+          
         let grupoTarifas = {
-          codigoActividad : simulacion.codigoActividad,
-          descripcionActividad: simulacion.descripcionActividad,
-          listaTarifas : this.tarifasClasificadas[simulacion.codigoActividad]
+          codigoActividad : simulacionExt.codigoActividad,
+          descripcionActividad: simulacionExt.descripcionActividad,
+          listaTarifas : this.tarifasClasificadas[simulacionExt.codigoActividad]
         }
-  
+    
         this.arrayTarifas.push(grupoTarifas)
       }
     });
 
-    console.log("m2")
-    console.log(this.arrayTarifas)
+    this.deduciblesExistentes.sort(this.ordenaPorCodigoDeducible);
+    this.planesExistentes.sort(this.ordenaPorCodigoActividad);
   }
 
   selecciona(fila, columna) {
-    console.log(fila, columna)
-
     this._filaTarifaSeleccionada = fila;
     this._columnaTarifaSeleccionada = columna;
   }
+
+  guardarPlanExistente(simulacion) {
+    let plan = {
+      codigoActividad: simulacion.codigoActividad,
+      descripcionActividad: simulacion.descripcionActividad
+    }
+
+    if(this.planesExistentes.findIndex(pl => pl.codigoActividad === plan.codigoActividad) === -1) {
+      this.planesExistentes.push(plan)
+    }
+  }
+
+  guardarDeducibleExistente(simulacion) {
+    let deducible = {
+      codigoDeducible: simulacion.codigoDeducible,
+      descripcionDeducible: simulacion.descripcionDeducible
+    }
+
+    if(this.deduciblesExistentes.findIndex(ded => ded.codigoDeducible === deducible.codigoDeducible) === -1) {
+      this.deduciblesExistentes.push(deducible)
+    }
+  }
+
+
+
 }
