47e284e5243db963dd41333b44b9237eabf41ad1
Mario Bonilla <mario.bonilla@reale.cl>
2020-02-11 18:34:06 -0300

    Comienzo simulacion Hogar

---
+++ angular-udemy/fernando_herrera/cotizadorAngular/src/app/components/products/inmobiliario/hogar/components/simulacion-hogar/simulacion-hogar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simulacion-hogar',
+  templateUrl: './simulacion-hogar.component.html',
+  styleUrls: ['./simulacion-hogar.component.css']
+})
+export class SimulacionHogarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular-udemy/fernando_herrera/cotizadorAngular/src/app/components/products/inmobiliario/hogar/hogar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SimulacionHogarComponent } from './components/simulacion-hogar/simulacion-hogar.component';
+
+
+
+@NgModule({
+  declarations: [SimulacionHogarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HogarModule { }
